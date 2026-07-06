9c92a540e090843bbf681dce522f8b37bcd4a8aa
2005Mc <chandanamadasu2005@gmail.com>
2026-07-06 15:48:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -95,60 +95,6 @@ topBtn.onclick=()=>{
 };
 
 
-/* ==========================================
-   COUNTER ANIMATION
-========================================== */
-
-const counters = document.querySelectorAll(".counter");
-
-const counterObserver = new IntersectionObserver(entries=>{
-
-entries.forEach(entry=>{
-
-if(entry.isIntersecting){
-
-const counter = entry.target;
-
-const target = +counter.dataset.target;
-
-let count = 0;
-
-const speed = target/100;
-
-const update = ()=>{
-
-count += speed;
-
-if(count < target){
-
-counter.innerText=Math.ceil(count);
-
-requestAnimationFrame(update);
-
-}else{
-
-counter.innerText=target;
-
-}
-
-}
-
-update();
-
-counterObserver.unobserve(counter);
-
-}
-
-});
-
-});
-
-counters.forEach(counter=>{
-
-counterObserver.observe(counter);
-
-});
-
 
 /* ==========================================
    SCROLL REVEAL
